f5fe14a51a70d01d24dc04a4b4622b10197349a2
Joe Crick <joe@bitovi.com>
2015-09-25 16:42:27 -0700

    Format gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -14,47 +14,47 @@ var reporters = require('jasmine-reporters');
 require('babel-core/register');
 
 var handleErr = function (err) {
-  console.log(err.message);
-  process.exit(1);
+    console.log(err.message);
+    process.exit(1);
 };
 
 gulp.task('static', function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(xo())
-    .on('error', handleErr);
+    return gulp.src('lib/**/*.js')
+        .pipe(excludeGitignore())
+        .pipe(xo())
+        .on('error', handleErr);
 });
 
 gulp.task('nsp', function (cb) {
-  nsp('package.json', cb);
+    nsp('package.json', cb);
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js').pipe(babel())
+    return gulp.src('lib/**/*.js').pipe(babel())
 
-    .pipe(istanbul({includeUntested: true}))
-    .pipe(istanbul.hookRequire());
+        .pipe(istanbul({includeUntested: true}))
+        .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', ['pre-test'], function (cb) {
-  var mochaErr;
+    var mochaErr;
 
-  gulp.src('test/**/*.js')
-    .pipe(plumber())
-    .pipe(jasmine({reporter: new reporters.JUnitXmlReporter()}))
-    .on('error', function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on('end', function () {
-      cb(mochaErr);
-    });
+    gulp.src('test/**/*.js')
+        .pipe(plumber())
+        .pipe(jasmine({reporter: new reporters.JUnitXmlReporter()}))
+        .on('error', function (err) {
+            mochaErr = err;
+        })
+        .pipe(istanbul.writeReports())
+        .on('end', function () {
+            cb(mochaErr);
+        });
 });
 
 gulp.task('babel', function () {
-  return gulp.src('lib/**/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    return gulp.src('lib/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('prepublish', ['nsp', 'babel']);
